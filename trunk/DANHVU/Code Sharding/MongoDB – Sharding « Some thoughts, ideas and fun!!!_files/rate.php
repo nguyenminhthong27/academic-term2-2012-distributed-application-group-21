a6b4ebc25c1090b1ac8221b5df84de911333c5de
phamdanhvu@gmail.com <phamdanhvu@gmail.com@f4f23139-97e4-e68c-d647-dd63de0779d2>
2012-05-12 09:34:32 +0000

    sharing code and introduction

---
+++ trunk/DANHVU/Code Sharding/MongoDB – Sharding « Some thoughts, ideas and fun!!!_files/rate.php
@@ -0,0 +1,30 @@
+PDRTJS_2791191_page_61.avg_rating = 4.5;
+PDRTJS_2791191_page_61.votes = 3;		
+PDRTJS_settings_2791191_page_61= {
+			"type" : "stars",
+			"size" : "sml",
+			"star_color" : "yellow",
+			"custom_star" : "",
+			"font_size" : "",
+			"font_line_height" : "16px",
+			"font_color" : "",
+			"font_align" : "left",
+			"font_position" : "right",
+			"font_family" : "",
+			"font_bold" : "normal",
+			"font_italic" : "normal",
+			"text_vote" : "Vote",
+			"text_votes" : "Votes",
+			"text_rate_this" : "Rate This",
+			"text_1_star" : "Very Poor",
+			"text_2_star" : "Poor",
+			"text_3_star" : "Average",
+			"text_4_star" : "Good",
+			"text_5_star" : "Excellent",
+			"text_thank_you" : "Thank You",
+			"text_rate_up" : "Rate Up",
+			"text_rate_down" : "Rate Down"
+		};
+PDRTJS_2791191_page_61.init();		
+PDRTJS_2791191_page_61.token='ed48f8efe208c876ef0ee286de92fbc3';
+/*2791191,_page_61,wp-page-61,1934488881,4.5-3*/
