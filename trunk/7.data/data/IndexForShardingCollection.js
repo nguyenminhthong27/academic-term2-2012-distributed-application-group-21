a4b17897460771f97627709ed04a1b996b0ae254
hathao298 <hathao298@f4f23139-97e4-e68c-d647-dd63de0779d2>
2012-05-16 07:26:34 +0000

    Submit script file for creating index on MaNH for sharding File upload: IndexForShardingCollection.js

---
+++ trunk/7.data/data/IndexForShardingCollection.js
@@ -0,0 +1,134 @@
+
+/** BanAn indexes **/
+db.getCollection("BanAn").ensureIndex({
+  "MaNH": 1
+},[
+  
+]);
+
+/** CaLamViec indexes **/
+db.getCollection("CaLamViec").ensureIndex({
+  "MaNH": 1
+},[
+  
+]);
+
+/** ChiTietDatCho indexes **/
+db.getCollection("ChiTietDatCho").ensureIndex({
+  "MaNH": 1
+},[
+  
+]);
+
+/** ChiTietHoaDon indexes **/
+db.getCollection("ChiTietHoaDon").ensureIndex({
+  "MaNH": 1
+},[
+  
+]);
+
+/** ChiTietHopDong indexes **/
+db.getCollection("ChiTietHopDong").ensureIndex({
+  "MaNH": 1
+},[
+  
+]);
+
+/** ChiTietKhoHang indexes **/
+db.getCollection("ChiTietKhoHang").ensureIndex({
+  "MaNH": 1
+},[
+  
+]);
+
+/** ChiTietThucDon_MonAn indexes **/
+db.getCollection("ChiTietThucDon_MonAn").ensureIndex({
+  "MaNH": 1
+},[
+  
+]);
+
+/** CongViec indexes **/
+db.getCollection("CongViec").ensureIndex({
+  "MaNH": 1
+},[
+  
+]);
+
+/** HinhThucThanhToan indexes **/
+db.getCollection("HinhThucThanhToan").ensureIndex({
+  "MaNH": 1
+},[
+  
+]);
+
+/** HoaDon indexes **/
+db.getCollection("HoaDon").ensureIndex({
+  "MaNH": 1
+},[
+  
+]);
+
+/** HopDong indexes **/
+db.getCollection("HopDong").ensureIndex({
+  "MaNH": 1
+},[
+  
+]);
+
+/** KhuVuc indexes **/
+db.getCollection("KhuVuc").ensureIndex({
+  "MaNH": 1
+},[
+  
+]);
+
+/** LichCongViec indexes **/
+db.getCollection("LichCongViec").ensureIndex({
+  "MaNH": 1
+},[
+  
+]);
+
+
+/** NhanVien indexes **/
+db.getCollection("NhanVien").ensureIndex({
+  "MaNH": 1
+},[
+  
+]);
+
+/** PhanCong indexes **/
+db.getCollection("PhanCong").ensureIndex({
+  "MaNH": 1
+},[
+  
+]);
+
+/** PhieuDatCho indexes **/
+db.getCollection("PhieuDatCho").ensureIndex({
+  "MaNH": 1
+},[
+  
+]);
+
+/** PhieuNhapHang indexes **/
+db.getCollection("PhieuNhapHang").ensureIndex({
+  "MaNH": 1
+},[
+  
+]);
+
+/** PhuongThucThanhToan indexes **/
+db.getCollection("PhuongThucThanhToan").ensureIndex({
+  "MaNH": 1
+},[
+  
+]);
+
+/** ThucDon indexes **/
+db.getCollection("ThucDon").ensureIndex({
+  "MaNH": 1
+},[
+  
+]);
