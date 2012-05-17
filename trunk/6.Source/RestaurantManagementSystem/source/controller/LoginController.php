ba271d3710b76734206c4eea2237405eb05ed9cf
vantuanlee@gmail.com <vantuanlee@gmail.com@f4f23139-97e4-e68c-d647-dd63de0779d2>
2012-05-17 10:46:42 +0000

    LOGIN, LOGOUT COMPLETE - LoginController : fix bugs on login() and source code - gui/home.php : check session for uname - module_login_logout.js : fix ajax and js - org.eclipse.php.debug.core.Debug_Process_Preferences.prefs : remove "Break at first line" when debug

--- trunk/6.Source/RestaurantManagementSystem/source/controller/LoginController.php
+++ trunk/6.Source/RestaurantManagementSystem/source/controller/LoginController.php
@@ -1,4 +1,5 @@
 <?php
+require_once '../dal/AccountDAO.php';
 /**
  * login handle
  * @author vantuanlee@gmail.com
@@ -7,38 +8,39 @@ class LoginController{
 
 	public $type = "MaLoaiNV";//temporary staff_type
 
-
 	/**
-	 *
-	 * */
+	 * @param string $username
+	 */
 	public function validateLoginForm($username){
 
 
 	}
 
 	/**
-	 * @author thanhtuan
 	 * main login method
+	 * @author thanhtuan
 	 * @param $username user name
 	 * @param $password pass word
-	 * @return login result
+	 * @return true if login successful and otherwise
 	 * */
 	public function login($username, $password) {
-		$result_suc = "Chào Mừng , " ;
-		$reusult_fail = "Login Failed";
-		$acount  = new AccountDAO();
-		$loginResult = $acount->validateLonginInfo($username,$password);
-		if(isset($loginResult)) {
-			session_start();
-			$_SESSION['is_login'] = true;
-			$_SESSION['staff_type'] = $loginResult[$type];
-			$_SESSION['uname'] = $username;
-			$result_suc = $result_suc. $username.'<a href="javascript:logout()">Logout';
-			return $result_suc;
+		$dao = new AccountDAO();
+		$account = $dao->getInfo($username);
+
+		// if account exists
+		if ($account != null) {
+			$psswd = $account["Password"];
+			// if login successful
+			if ($psswd === $password) {
+				session_start();
+				$_SESSION['is_login'] = true;
+				$_SESSION['staff_type'] = $account["LoaiNV"];
+				$_SESSION['uname'] = $username;
+				return true;
+			}
 		}
-		else {
-			return $reusult_fail;
-		}
+		// if login unsuccessful
+		return false;
 	}
 
 	/**
@@ -52,29 +54,23 @@ class LoginController{
 
 // get action form request params
 $action = isset($_REQUEST["action"]) ? $_REQUEST["action"] : "";
-switch ($action) {
-	case "login":
-		// check login
-		$username = isset($_REQUEST["uname"]) ? $_REQUEST["uname"] : "";
-		$password = isset($_REQUEST["psswd"]) ? $_REQUEST["psswd"] : "";
+if ($action == "login") {
+	// check login
+	$username = isset($_REQUEST["uname"]) ? $_REQUEST["uname"] : "";
+	$password = isset($_REQUEST["psswd"]) ? $_REQUEST["psswd"] : "";
 
-		if ($username != "" && $password != "") {
-			try {
-				// do login
-				$loginCtrl = new LoginController();
-				$loginResult = $loginCtrl->login($username, $password);
-				echo $loginResult;
+	if ($username != "" && $password != "") {
+		try {
+			// do login
+			$loginCtrl = new LoginController();
+			$loginResult = $loginCtrl->login($username, $password);
+			echo $loginResult;
 
-			} catch (Exception $e) {
-				echo "Login Failed";
-			}
-		} else {
+		} catch (Exception $e) {
 			echo "Login Failed";
 		}
-		break;
-
-	default:
-		break;
+	} else {
+		echo "Login Failed";
+	}
 }
-
 ?>
