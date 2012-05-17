2bc65bf33913094429bb22827413a312d5bf0ea1
vantuanlee@gmail.com <vantuanlee@gmail.com@f4f23139-97e4-e68c-d647-dd63de0779d2>
2012-05-17 05:33:38 +0000

    - add new interface IDatabaseConfig.php for data configuration - Class MongoDatabase.php :  Implement from interface IDatabaseConfig.php - AccountDAO : getInfo() complete - org.eclipse.core.resources.prefs : fix encoding cpl25 to utf-8 - remove redundant file : PermissionDAO.php

--- trunk/6.Source/RestaurantManagementSystem/source/dal/AccountDAO.php
+++ trunk/6.Source/RestaurantManagementSystem/source/dal/AccountDAO.php
@@ -1,10 +1,19 @@
 <?php
+require_once 'MongoDatabase.php';
 /**
  * all methods that are relative to account object.
  * @author vantuanlee@gmail.com
  * @created May 11 2012	
  * */
 class AccountDAO {
-	
+			
+	/**
+	 * @param string $username
+	 * @return NULL
+	 */
+	public function getInfo($staffId) {
+		$condition = 'MaNV='. $staffId;
+		return MongoDatabase::getOneDataFrom("NhanVien", $condition);		
+	}
 } 
 ?>
---
+++ trunk/6.Source/RestaurantManagementSystem/source/dal/IDatabaseConfig.php
@@ -0,0 +1,8 @@
+<?php
+interface IDatabaseConfig {
+	const Host = "localhost:27021";
+	const DbName = "Restaurant_system_db";
+	const UserName = "";
+	const Password = "";
+}
+?>
