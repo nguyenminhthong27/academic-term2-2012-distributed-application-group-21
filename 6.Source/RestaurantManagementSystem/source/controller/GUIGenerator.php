0e9f926ec9f7336f4b4e66c9e014659170c75c7a
vantuanlee <vantuanlee@gmail.com>
2012-05-18 15:22:45 +0000

    - add GUI for billing functions. - GUIGenerator.php :  Generate UIs - booking.php : fix UIs code

--- 6.Source/RestaurantManagementSystem/source/controller/GUIGenerator.php
+++ 6.Source/RestaurantManagementSystem/source/controller/GUIGenerator.php
@@ -1,9 +1,49 @@
 <?php
+require_once '../dal/RestaurantDAO.php';
 /**
  * user interface generator
  * generate view
  * */
 class GUIGenerator {
 	
+	/**
+	 * show select box restaurants
+	 */
+	public static function htmlShowRestaurantSelect(){
+		$currentRes = $_SESSION["restaurant"];
+		
+		$dao = new RestaurantDAO();
+		$restaurants = $dao->getAllRestaurant();
+		
+		$html = '
+		<select id="selectRestaurant">
+			<option value="'. $currentRes .'" selected>Nội bộ</option>
+			<option value="-1">Tất cả</option>';
+		
+		foreach ($restaurants as $rest){
+			$html .= '<option value="'. $rest["MaNH"] . '">' . $rest["TenNH"] . '</option>';
+		}
+		
+		$html .= '</select>';
+		
+		return $html;
+	}
+	
+	/**
+	 * show select box area 
+	 */
+	public static function htmlShowAreaSelect(){
+		$areas = MongoDatabase::getAllDataFrom("KhuVuc");
+		$html = '
+		<select id="selectArea">
+			<option value="-1" selected>Tất cả</option>';
+		
+		foreach ($areas as $area){
+			$html .= '<option value="'. $area["MaKV"] . '">' . $area["TenKV"] . '</option>';
+		}
+		$html .= '</select>';
+		
+		return $html;
+	}
 } 
 ?>
--- 6.Source/RestaurantManagementSystem/source/gui/tableManagement.php
+++ 6.Source/RestaurantManagementSystem/source/gui/tableManagement.php
@@ -1,151 +1,97 @@
-<?php
-//check is_login
-session_start();
-$is_login = isset($_SESSION['is_login']) ? $_SESSION['is_login'] : false;
-$uname = isset($_SESSION['uname']) ? $_SESSION['uname'] : "";
-$staff_type = isset($_SESSION['staff_type']) ? $_SESSION['staff_type'] : "";
 
-// check login
-if ($is_login == false) {
-	require_once '../configure/GeneralFunctions.php';
-	echo GeneralFunctions::Alert("Bạn chưa đăng nhập.");
-	header("Location: index.php");
-}
-
-// hard code to test staff_type
-$staff_type = "NV Thu Ngan";
-if ($staff_type != "NV Thu Ngan") {
-	require_once '../configure/GeneralFunctions.php';
-	echo GeneralFunctions::Alert("Bạn không có đủ quyền để thực hiện chức năng này.");
-	header("Location: home.php");
-}
-
-require_once '../configure/IncludeGenerator.php';
-require_once '../controller/GUIGenerator.php';
-
-?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
     <head>
         <meta http-equiv="Content-type" content="text/html; charset=utf-8" />
         <title>T4V RESTAURANT</title>
         <link rel="stylesheet" href="../css/style.css" type="text/css" media="all" />
+        <link rel="stylesheet" href="../css/tableManagement.css" type="text/css" media="all" />
         <link rel="stylesheet" href="../css/start/jquery-ui-1.8.20.custom.css" type="text/css" media="all" />
         <link rel="stylesheet" href="../css/jquery-ui-timepicker-addon.css" type="text/css" media="all" />
-        <link rel="stylesheet" href="../css/seatManagement.css" type="text/css" media="all" />
+        <link rel="stylesheet" href="../css/fancybox/jquery.fancybox-1.3.1.css" type="text/css" media="all" />
+        
         <script src="../js/lib/jquery-1.7.2.min.js" type="text/javascript"></script>
         <script src="../js/lib/jquery.jcarousel.pack.js" type="text/javascript"></script>
         <script src="../js/lib/jquery-ui-1.8.20.custom.min.js" type="text/javascript"></script>
         <script src="../js/lib/jquery-ui-timepicker-addon.js" type="text/javascript"></script>
-        
-        <script src="../js/module_booking.js" type="text/javascript"></script>
-        
+        <script src="../js/tableManagementFunc.js" type="text/javascript"></script>
     </head>
     <body>
         <div id="page" class="shell">
             <!-- Logo + Search + Navigation -->
-            <?php
-            echo IncludeGenerator::LogoGenerate(); 
-            ?>
-            <!-- END Logo + Search + Navigation -->                                  
+            <div id="top">
+                <div class="cl">&nbsp;</div>
+                <h1 id="logo"><a href="#">T4V RESTAURANT</a></h1>          
+                <div class="cl">&nbsp;</div>
+                <div id="navigation">
+                    <ul>
+                        <li><a href="#" class="active"><span>Trang chủ</span></a></li>                        
+                        <li><a href="#"><span>Giới thiệu</span></a></li>
+                        <li><a href="#"><span>Liên hệ</span></a></li>
+                    </ul>
+                </div>	
+            </div>
+            <!-- END Logo + Search + Navigation -->
+            <!-- Header -->
+            <div id="header">
+            </div>
+            <!-- END Header -->                        
             <!-- Main -->
             <div id="main">	
                 <!-- Menu -->
-                <div ><img id="button-show" title="Click here" src="../css/images/button-next.gif"/></div>
+                <div ><img id="button-show" title="Click here" src="css/images/button-next.gif"/></div>
                 <div id="menuDiv" class="menu" style="display:none">
                     <ul>
-                        <li><a href="javascript:booking()"><img src="../css/images/plusIcon.png" title="Đặt chỗ"/></a></li>
-                        <li><a href="javascript:showTableList()"><img src="../css/images/tableIcon.png" title="Danh sách bàn"/></a></li>
+                        <li><a href="#"><img src="css/images/plusIcon.png" title="Đặt chỗ"/></a></li>
+                        <li><a href="#"><img src="css/images/tableIcon.png" title="Danh sách bàn"/></a></li>
                     </ul>
                 </div>
-                <!-- END Menu -->             
+                <!-- END Menu -->
                 <div class="main-content">
-                    <div id="main-header">ĐẶT CHỖ</div>
-                    <div class="customerInfo">
-                        <table>
-                            <tr>
-                                <td>Họ tên khách hàng</td>
-                                <td><input type="text"></input></td>
-                            </tr>
-                            <tr>
-                                <td>Chứng minh nhân dân</td>
-                                <td><input type="text"></input></td>
-                            </tr>
-                            <tr>
-                                <td>Số điện thoại</td>
-                                <td><input type="text"></input></td>
-                            </tr>
-                        </table>
-                    </div>
-                    <div class="bookingDetailInfo">
-                        <table id="bookingDetailInfoTable">
-                            <tr>
-                                <td>Mã bàn ăn</td>
-                                <td><input type="text"></input></td><br/>
-                            </tr>
-                            <tr>
-                                <td>Từ</td>
-                                <td><input type="text" class="fromDtPker"></input></td>
-                                <td>Đến</td>
-                                <td><input type="text" class="toDtPker"></input></td>
-                            </tr>
-                        </table>                        
-                    </div>
-                    <div class="buttonToolBar">
-                        <button>Lưu</button>
-                        <button onclick="addTable();">Thêm bàn ăn</button>
-                        <button id="searchTableBut">Tra cứu bàn ăn</button>
-                    </div>
-                </div>              
-                <!-- END Main content -->
-            </div>
-            <!-- END Main -->
-            <!-- Footer -->
-             <?php 
-             echo IncludeGenerator::FooterGenerate();
-             ?>
-            <!-- END Footer -->        
-            <!-- Addtional -->
-            <!-- Table Info Dialog -->
-            <div class="tableInfoDialog">
-                <div class="main-content-dialog">
                     <!-- Search food table -->
                     <div class="search-table">
                         <table>
                             <tr>
                                 <td>Nhà hàng</td>
                                 <td>
-                                    <?php
-                                    echo GUIGenerator::htmlShowRestaurantSelect(); 
-                                    ?>
+                                    <select>
+                                        <option selected>Nội bộ</option>
+                                        <option>Tất cả</option>
+                                        <option>A</option>
+                                        <option>B</option>
+                                    </select>
                                 </td>                      
                             </tr>
                             <tr>
                                 <td>Khu vực</td>
                                 <td>
-                                    <?php echo GUIGenerator::htmlShowAreaSelect();?>
+                                    <select>
+                                        <option selected>Tất cả</option>
+                                        <option>Bình thường</option>
+                                        <option>VIP</option>
+                                    </select>
                                 </td>
                                 <td>Tình trạng</td>
                                 <td><select>
-                                        <option value="-1" selected>Tất cả</option>
-                                        <option value="0">Chưa đặt</option>
-                                        <option value="1">Đã đặt</option>                        
+                                        <option value="allStt" selected>Tất cả</option>
+                                        <option value="availStt">Chưa đặt</option>
+                                        <option value="notAvailStt">Đã đặt</option>                        
                                     </select></td>
                             </tr>
                             <tr>
                                 <td>Từ</td>
                                 <td>
-                                    <input type="text" class="fromDtPker"></input>
+                                    <input type="text" id="fromDtPker"></input>
                                 </td>
                                 <td>Đến</td>
                                 <td>
-                                    <input type="text" class="toDtPker"></input><br/>
+                                    <input type="text" id="toDtPker"></input><br/>
                                 </td>
                             </tr>
                             <tr>
                                 <td></td>
                                 <td></td>
-                                <td><button onclick="javascript:searchTable()">Tìm kiếm</button></td>
+                                <td><button>Tìm kiếm</button></td>
                             </tr>
                         </table>                                                
 
@@ -189,6 +135,7 @@ require_once '../controller/GUIGenerator.php';
                                 <th>GIÁ THÀNH</th>
                                 <th>SỐ NGƯỜI</th>
                                 <th>TÌNH TRẠNG</th>
+                                <th>NHÀ HÀNG</th>
                             </tr>
                             <tr>
                                 <td>123</td>
@@ -196,6 +143,7 @@ require_once '../controller/GUIGenerator.php';
                                 <td>1.200.000</td>
                                 <td>10</td>
                                 <td>Chưa đặt</td>
+                                <td>A</td>
                             </tr>       
                             <tr>
                                 <td>123</td>
@@ -205,15 +153,28 @@ require_once '../controller/GUIGenerator.php';
                                 <td>1.200.000</td>
                                 <td>10</td>
                                 <td><a onclick="bookingDetailLinkClicked();" href="#">Chi tiết</a></td>
-                            </tr>
+                                <td>B</td>
+                            </tr>       
                         </table>
 
                     </div>
-                </div>
-                <!-- END table of food table -->
-            </div>  
-            <!-- END table info Dialog -->
+                    <!-- END table of food table -->
+                </div>                
+            </div>
+            <!-- END Main -->
+            <!-- Footer -->
+            <div id="footer">
+                <p class="right">&copy; 2012 - T4V Restaurant &nbsp; Design by T4V Group
+                    <p>
+                        <a href="#">Trang chủ</a><span>&nbsp;</span>
+                        <a href="#">Giới thiệu</a><span>&nbsp;</span>
+                        <a href="#">Liên hệ</a><span>&nbsp;</span>
+                        <div class="cl">&nbsp;</div>
+                    </p>
+                </p>
+            </div>
+            <!-- END Footer -->
             <br />
+        </div>
     </body>
-
 </html>
