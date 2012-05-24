7aecdc6cf0ee7f3b00c284f95e374f16ee1575d3
vantuanlee@gmail.com <vantuanlee@gmail.com@f4f23139-97e4-e68c-d647-dd63de0779d2>
2012-05-24 03:21:59 +0000

    SEARCH BOOKING IN CREATE BILL COMPLETE. - ModuleCreateBillController.php : fix call methods, and making $to from $from. - BookingDAO : search() and createConditionToSearch() - module_create_bill.js : fix bugs call jQuery [using document.getElementById instead of jQuery]

--- trunk/6.Source/RestaurantManagementSystem/source/controller/ModuleCreateBillController.php
+++ trunk/6.Source/RestaurantManagementSystem/source/controller/ModuleCreateBillController.php
@@ -1,10 +1,11 @@
 <?php
 // add require CreateBillDAO
 
+
 class CreateBillController{
-	
-	
-	
+
+
+
 	/**
 	 * method  getListDish() get all Dish of restaurants
 	 * @param no
@@ -16,27 +17,27 @@ class CreateBillController{
 		$arr = $dao->getListDishDAO();
 		$data = "";
 		$data = "table>
-         <tr>
-         <th><input type='checkbox' id='checkAllCBox' onclick='checkAllCBoxClicked();'></input></th>
-         <th>Món ăn</th>
-         <th>Giá thành</th>
-          </tr>";
+		<tr>
+		<th><input type='checkbox' id='checkAllCBox' onclick='checkAllCBoxClicked();'></input></th>
+		<th>Món ăn</th>
+		<th>Giá thành</th>
+		</tr>";
 		foreach ($arr as $value){
-		
-		$TenMoAn = isset($value["TenMonAn"]) ? $value["TenMonAn"] : "";
-		$DonGia = isset($value["DonGia"]) ? $value["DonGia"] : "";
-		$data = $data."<tr>";
-		$data = $data."<td><input type='checkbox'></input></td>";
-		$data = $data."<td>$TenMonAn</td>";
-		$data = $data."<td>$DonGia</td>";
-		$data = $data."</tr>";
+
+			$TenMoAn = isset($value["TenMonAn"]) ? $value["TenMonAn"] : "";
+			$DonGia = isset($value["DonGia"]) ? $value["DonGia"] : "";
+			$data = $data."<tr>";
+			$data = $data."<td><input type='checkbox'></input></td>";
+			$data = $data."<td>$TenMonAn</td>";
+			$data = $data."<td>$DonGia</td>";
+			$data = $data."</tr>";
 		}
 		$data = $data."</table>";
 		return $data;
-		
+
 	}
 	/**
-	 * method  searchBooking
+	 * method  searchBooking by date, customer name, customer id, customner phone
 	 * @param $date
 	 * @param $customerName
 	 * @param $id (CMND)
@@ -45,65 +46,70 @@ class CreateBillController{
 	 * @author thanhtuan
 	 */
 	public  function searchBooking($date,$customerName,$id,$numberphone){
-		$dao = new CreateBillDAO();
-		$arr = $dao->geBookingDAO($date,$customerName,$id,$numberphone);
+		// make $to from $from
+		$from = new DateTime($date);
+		$to = new DateTime($date);
+		$to->modify("+1 day");
+		
+		$dao = new BookingDAO();
+		$arr = $dao->search($from->format('Y-m-d H:i:s'), $to->format('Y-m-d H:i:s'), $customerName, $id);
 		$data = "";
 		$data = $data."<table>
 		<table>
-        <tr>
-        <th>Mã phiếu</th>
-        <th>Ngày lập</th>
-        <th>Họ tên khách hàng</th>
-        <th>Số CMND</th>
-        <th>Số điện thoại</th>
-        <th>Người tiếp nhận</th>
-        </tr>";
+		<tr>
+		<th>Mã phiếu</th>
+		<th>Ngày lập</th>
+		<th>Họ tên khách hàng</th>
+		<th>Số CMND</th>
+		<th>Số điện thoại</th>
+		<th>Người tiếp nhận</th>
+		</tr>";
 		foreach($arr as $value){
-		$MaPhieu = isset($value["MaPhieu"]) ? $value["MaPhieu"] : "";
-		$NgayLap = isset($value["NgayLap"]) ? $value["NgayLap"] : "";
-		$HoTenKH = isset($value["HoTenKH"]) ? $value["HoTenKH"] : "";
-		$CMND = isset($value["CMND"]) ? $value["CMND"] : "";
-		$SDT = isset($value["SDT"]) ? $value["SDT"] : "";
-		$NguoiTiepNhan = isset($value["TenNV"]) ? $value["TenNV"] : "";
-        $data = $data."<tr>";
-        $data = $data."<td>$MaPhieu</td>";
-        $data = $data."<td>$NgayLap</td>";
-        $data = $data."<td>$HoTenKH</td>";
-        $data = $data."<td>$CMND</td>";
-        $data = $data."<td>$SDT</td>";
-        $data = $data."<td>$NguoiTiepNhan</td>";
-        $data = $data."</tr>";
+			$MaPhieu = isset($value["MaPhieu"]) ? $value["MaPhieu"] : "";
+			$NgayLap = isset($value["NgayLap"]) ? $value["NgayLap"] : "";
+			$HoTenKH = isset($value["HoTenKH"]) ? $value["HoTenKH"] : "";
+			$CMND = isset($value["CMND"]) ? $value["CMND"] : "";
+			$SDT = isset($value["SDT"]) ? $value["SDT"] : "";
+			$NguoiTiepNhan = isset($value["NguoiTiepNhan"]) ? $value["NguoiTiepNhan"] : "";
+			$data = $data."<tr>";
+			$data = $data."<td>$MaPhieu</td>";
+			$data = $data."<td>$NgayLap</td>";
+			$data = $data."<td>$HoTenKH</td>";
+			$data = $data."<td>$CMND</td>";
+			$data = $data."<td>$SDT</td>";
+			$data = $data."<td>$NguoiTiepNhan</td>";
+			$data = $data."</tr>";
 		}
 		$data = $data."</table>";
 		return $data;
-				
+
 	}
 	/*
 	 * @author: thanhtuan
-	 * method saveBill save a bill with status = 0(do DAO status = 0)
-	 * @param: $maHD string
-	 * @param: $tongTien : float (sum money of all bills)
-	 *  @param : $ngaLap date (date of) 
-	 * @param :maNH string (ID of restaurant) 
-	 * @param:maPhieuDatCho : string (ID of booking) 
-	 * @param: $tenMonAn string
-	 * $param: $soLuong integer
-	 * return true if succes else return false
-	 */
+	* method saveBill save a bill with status = 0(do DAO status = 0)
+	* @param: $maHD string
+	* @param: $tongTien : float (sum money of all bills)
+	*  @param : $ngaLap date (date of)
+	* @param :maNH string (ID of restaurant)
+	* @param:maPhieuDatCho : string (ID of booking)
+	* @param: $tenMonAn string
+	* $param: $soLuong integer
+	* return true if succes else return false
+	*/
 	public function saveBill($maHD,$tongTien,$ngayLap,$nguoiLap,$maNH,$maPhieuDatCho,$tenMonAn,$soLuong){
-		
+
 		try {
-			
-			
-		$dao = new CreateBillDAO();
-		$bool = $dao->checkBooking($maPhieuDatCho);//check exist of card booking,recive value = true if card booking exist else value = false
-		if($bool== true)
-		{
-		return $dao->save($maHD,$tongTien,$ngayLap,$nguoiLap,$maNH,$maPhieuDatCho,$tenMonAn,$soLuong);
-		}
-		else 
-			return false;
-		
+				
+				
+			$dao = new CreateBillDAO();
+			$bool = $dao->checkBooking($maPhieuDatCho);//check exist of card booking,recive value = true if card booking exist else value = false
+			if($bool== true)
+			{
+				return $dao->save($maHD,$tongTien,$ngayLap,$nguoiLap,$maNH,$maPhieuDatCho,$tenMonAn,$soLuong);
+			}
+			else
+				return false;
+
 		}
 		catch(Exception $e) {
 			echo "Not Connect to database";
@@ -112,7 +118,7 @@ class CreateBillController{
 	}
 	/*
 	 * @author thanhtuan
-	 * method change format of a date
+	* method change format of a date
 	* @param $datime string
 	* @return string $datetime after format.
 	*/
@@ -126,8 +132,8 @@ class CreateBillController{
 $action = isset($_REQUEST["action"]) ? $_REQUEST["action"] : "";
 switch ($action) {
 	case "searchListDish":
-		
-		
+
+
 		try {
 			// do search
 			$search = new CreateBillController();
@@ -137,56 +143,55 @@ switch ($action) {
 			echo "Not Connect to database";
 		}
 		break;
-
 	case "searchBooking":
 		$date = isset($_REQUEST["date"]) ? $_REQUEST["date"] : "";
-		$customerName= isset($_REQUEST["cus"]) ? $_REQUEST["cus"] : "";
-		$id = isset($_REQUEST["id"]) ? $_REQUEST["id"] : "";
-		$numberphone= isset($_REQUEST["num"]) ? $_REQUEST["num"] : "";
+		$customerName= isset($_REQUEST["cus_name"]) ? $_REQUEST["cus_name"] : "";
+		$customerID = isset($_REQUEST["cus_id"]) ? $_REQUEST["cus_id"] : "";
+		$customerPhone= isset($_REQUEST["cus_phone"]) ? $_REQUEST["cus_phone"] : "";
 		if($date != ""){
 			$format = new CreateBillController();
 			$date = $format->changeFormatDate($date);
-			
 		} else {
 			echo "Bạn phải chọn thời gian tìm kiếm.";
 			continue;
 		}
+
 		try {
 			// do search
 			$search = new CreateBillController();
-			$result = $search->searchBooking($date, $customerName, $id, $numberphone);
+			$result = $search->searchBooking($date, $customerName, $customerID, $customerPhone);
 			echo $SearchResult;
 		} catch (Exception $e) {
 			echo "Not Connect to database";
 		}
 		break;
-		case "saveBill":
-			$tongTien = isset($_REQUEST["sumMoney"]) ? $_REQUEST["sumMoney"] : "";
-			$date = isset($_REQUEST["date"]) ? $_REQUEST["date"] : "";
-			$maPhieuDatCho= isset($_REQUEST["idCardBooking"]) ? $_REQUEST["idCardBooking"] : "";
-			$tenMonAn = isset($_REQUEST["dish"]) ? $_REQUEST["dish"] : "";
-			$soLuong= isset($_REQUEST["number"]) ? $_REQUEST["number"] : "";
-			$maNH = $_SESSION["restaurant"];
-			$nguoiLap = $_SESSION["staff_id"];
-			$maHD = time();
-			if($date != ""){
-				$format = new CreateBillController();
-				$date = $format->changeFormatDate($date);
-		
-			} else {
-				echo "Bạn phải chọn thời gian nhập vào!";
-				continue;
-			}
-			try {
-				// do save
-				$save = new CreateBillController();
-				$result = $save->saveBill($maHD, $tongTien, $ngayLap, $nguoiLap, $maNH, $maPhieuDatCho, $tenMonAn, $soLuong);
-				echo $result;
-			} catch (Exception $e) {
-				echo "Not Connect to database";
-			}
-			break;
-		default:
-			break;
+	case "saveBill":
+		$tongTien = isset($_REQUEST["sumMoney"]) ? $_REQUEST["sumMoney"] : "";
+		$date = isset($_REQUEST["date"]) ? $_REQUEST["date"] : "";
+		$maPhieuDatCho= isset($_REQUEST["idCardBooking"]) ? $_REQUEST["idCardBooking"] : "";
+		$tenMonAn = isset($_REQUEST["dish"]) ? $_REQUEST["dish"] : "";
+		$soLuong= isset($_REQUEST["number"]) ? $_REQUEST["number"] : "";
+		$maNH = $_SESSION["restaurant"];
+		$nguoiLap = $_SESSION["staff_id"];
+		$maHD = time();
+		if($date != ""){
+			$format = new CreateBillController();
+			$date = $format->changeFormatDate($date);
+
+		} else {
+			echo "Bạn phải chọn thời gian nhập vào!";
+			continue;
+		}
+		try {
+			// do save
+			$save = new CreateBillController();
+			$result = $save->saveBill($maHD, $tongTien, $ngayLap, $nguoiLap, $maNH, $maPhieuDatCho, $tenMonAn, $soLuong);
+			echo $result;
+		} catch (Exception $e) {
+			echo "Not Connect to database";
+		}
+		break;
+	default:
+		break;
 }
 ?>
