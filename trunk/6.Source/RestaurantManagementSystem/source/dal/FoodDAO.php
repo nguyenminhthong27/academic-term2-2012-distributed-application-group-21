5d5a00734d8dff39ea962de72d40015908571e47
vantuanlee@gmail.com <vantuanlee@gmail.com@f4f23139-97e4-e68c-d647-dd63de0779d2>
2012-06-08 00:35:26 +0000

    ReportDAO : report by food COMPLETE

--- trunk/6.Source/RestaurantManagementSystem/source/dal/FoodDAO.php
+++ trunk/6.Source/RestaurantManagementSystem/source/dal/FoodDAO.php
@@ -9,6 +9,11 @@ class FoodDAO{
 		return MongoDatabase::getAllDataFrom("MonAn");		
 	}
 	
+	/**
+	 * @param unknown_type $fromDate
+	 * @param unknown_type $toDate
+	 * @return multitype:multitype:unknown Ambigous <unknown>  
+	 */
 	public function getListFoodWithUnitPrice($fromDate, $toDate){
 		// get menus on date.
 		$keys = array(
@@ -29,6 +34,7 @@ class FoodDAO{
 		$keys1 = array(
 				"_id" => 0,
 				"MaMonAn" => 1,
+				"MaChiTiet" => 1,
 				"DonGia" => 1
 				);
 		foreach($menus as $menu){			
@@ -38,6 +44,7 @@ class FoodDAO{
 							),
 					 $keys1);
 			foreach($foodWithUnitPrice as $food_menu){
+				$temp["MaChiTietThucDon_MonAn"] = $food_menu["MaChiTiet"];
 				$temp["DonGia"] = $food_menu["DonGia"];
 				$temp["MaMonAn"] = $food_menu["MaMonAn"];
 				$food = MongoDatabase::getOneDataFrom("MonAn", "MaMonAn=". $food_menu["MaMonAn"]);
@@ -48,10 +55,40 @@ class FoodDAO{
 		
 		return $result;
 	}
+	
+	/**
+	 * @param unknown_type $MaCTThucDon_MonAn
+	 */
+	public function getFoodByMenuFoodDetailID($MaCTThucDon_MonAn){
+		$menuFoodDetail = MongoDatabase::getOneDataFrom("ChiTietThucDon_MonAn", "MaChiTiet=" . $MaCTThucDon_MonAn);
+		
+		$result = array();
+		$result["MaChiTietThucDon_MonAn"] =  $menuFoodDetail["MaChiTiet"];
+		$result["DonGia"] =  $menuFoodDetail["DonGia"];
+		$result["TenMonAn"] = $this->getFoodName($menuFoodDetail["MaMonAn"]);
+		
+		return $result;
+	}
+	
+	/**
+	 * Get food name by food id
+	 * @param unknown_type $foodID
+	 * @return Ambigous <unknown>
+	 */
+	private function getFoodName($foodID){
+		$data = MongoDatabase::getOneDataFrom("MonAn", "MaMonAn=" . $foodID);
+		return $data["TenMonAn"];
+	}
+	
+	public function getFoodAmout($MaCTThucDon_MonAn){
+		$food = MongoDatabase::getOneDataFrom("ChiTietHoaDon", "MaCTTD_MA=" . $MaCTThucDon_MonAn);
+		return $food["SoLuong"];
+	}
 } 
 
 // hard code to test
 // $dao = new FoodDAO();
-// $data = $dao->getListFoodWithUnitPrice("2011-05-24", "2014-05-25");
+// $data = $dao->getFoodAmout("CT_TD_MA01");
+// echo $data;
 // print_r($data);
 ?>
